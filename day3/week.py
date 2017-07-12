8109396dac06d5e9a5981fa382f0e9aa8da2e92a
sai venkat <saivenkat.goluguri@quantiphi.com>
2017-07-12 16:07:51 +0530

    conflict

--- day3/week.py
+++ day3/week.py
@@ -1,9 +1,21 @@
-days=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-print("enter the day number you want")
-x=str(raw_input())
-if x.lower() not in days :
+print("do you want month or day name")
+y=str(raw_input())
+if (y=='day')
+    days=['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+    print("enter the day number you want")
+    x=str(raw_input())
+    if x.lower() not in days :
       print("day is not available")
-else:
+    else:
       num=days.index(x.lower())
+      print(num+1)
+if(y=='month')
+    month=['january','february','march','april','may','june','july','august','september','october','november']
+    print("enter the day number you want")
+    x=str(raw_input())
+    if x.lower() not in days :
+      print("day is not available")
+    else:
+      num=month.index(x.lower())
       print(num+1)
       
